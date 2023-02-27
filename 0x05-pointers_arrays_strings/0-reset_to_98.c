138aea610e88ccfa2e5ba847cf67c3fd04d34999
adeola231 <xxgreg234@yahoo.com>
2023-02-27 14:16:37 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - a funtcion that takes an int as parameter
+ * and updates the vakue it point to 98
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
